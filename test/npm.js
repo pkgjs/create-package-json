e875d469f1f5d420891feaac6fd265f1caa77bc0
Zack Schuster <email@zackschuster.com>
2021-10-08 22:12:49 -0700

    chore: migrate to semistandard

--- test/npm.js
+++ test/npm.js
@@ -1,22 +1,22 @@
-const assert = require('assert')
-const { suite, test } = require('mocha')
+const assert = require('assert');
+const { suite, test } = require('mocha');
 
-const npm = require('../lib/npm')
+const npm = require('../lib/npm');
 
 suite('npm', () => {
   test('accept empty semver', () => {
-    assert.doesNotThrow(() => npm.normalizePackageName('@pkgjs/create'))
-  })
+    assert.doesNotThrow(() => npm.normalizePackageName('@pkgjs/create'));
+  });
   test('accept valid semver', () => {
-    assert.doesNotThrow(() => npm.normalizePackageName('@pkgjs/create@1.0.0'))
-    assert.doesNotThrow(() => npm.normalizePackageName('@pkgjs/create@<1'))
-  })
+    assert.doesNotThrow(() => npm.normalizePackageName('@pkgjs/create@1.0.0'));
+    assert.doesNotThrow(() => npm.normalizePackageName('@pkgjs/create@<1'));
+  });
   test('accept valid semver (exceptions)', () => {
-    assert.doesNotThrow(() => npm.normalizePackageName('@pkgjs/create@<=2'))
-    assert.doesNotThrow(() => npm.normalizePackageName('@pkgjs/create@>=2'))
-    assert.doesNotThrow(() => npm.normalizePackageName('@pkgjs/create@*'))
-  })
+    assert.doesNotThrow(() => npm.normalizePackageName('@pkgjs/create@<=2'));
+    assert.doesNotThrow(() => npm.normalizePackageName('@pkgjs/create@>=2'));
+    assert.doesNotThrow(() => npm.normalizePackageName('@pkgjs/create@*'));
+  });
   test('reject invalid semver', () => {
-    assert.throws(() => npm.normalizePackageName('@pkgjs/create@a.b.c'))
-  })
-})
+    assert.throws(() => npm.normalizePackageName('@pkgjs/create@a.b.c'));
+  });
+});
